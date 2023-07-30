30d5e1dd812593b41631310996677f3763834b99
utkarsh8853 <utkarsh.pandey@appinventiv.com>
2023-07-30 14:08:39 +0530

    FIX:BRANCH ISSUE

--- advertisement_system/dist/app1.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// import express from "express";
-// const app:Express = express();
-// app.use(express.json());
-// // const port = 6000;
-// app.listen(3000,()=>{
-//   console.log("server are running on port 3000")
-//     })
